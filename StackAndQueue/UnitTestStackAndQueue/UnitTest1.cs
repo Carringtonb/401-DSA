d78607eedc54de71eeacdbbec21f01768936a5f5
carrington <carringtonbeard@live.com>
2020-03-31 08:58:07 -0700

    git dumpster fire

---
+++ StackAndQueue/UnitTestStackAndQueue/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using StackAndQueue.Classes
+using System.Collections;
+
+namespace UnitTestStackAndQueue
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CanCreateANewStack()
+        {
+            Stack stack = new Stack();
+            string expected = stack.ToString();
+            string input = "Top of stack -> Null";
+            Assert.Equal(expected, input);
+        }
+    }
+}
