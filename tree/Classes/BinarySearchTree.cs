9a75cff4c574ef39785162cd50eddfbe9ac1967c
carrington <carringtonbeard@live.com>
2020-04-06 10:49:06 -0700

    tree challenge

---
+++ tree/Classes/BinarySearchTree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Tree.Classes
+{
+    public class BinarySearchTree
+    {
+        public Node Root;
+
+        public Node insertNode(Node root, int value)
+        {
+            if (root == null)
+            {
+                root = new Node();
+                root.Value = value;
+            }else if (value < root.Value)
+            {
+                root.Left = insertNode(root.Left, value);
+            }
+            else
+            {
+                root.Right = insertNode(root.Right, value);
+            }
+            this.Root = root;
+            return this.Root;
+        }
+
+        public void traverse(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            traverse(root.Left);
+            traverse(root.Right);
+        }
+     
+    }
+}
---
+++ tree/Classes/BinaryTree.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tree.Classes
+{
+    public class Tree
+    {
+        public Node root { get; set; }
+
+        public Tree()
+        {
+            root = null;
+        }
+
+        public List<int> PreOrder()
+        {
+            Node Root = root;
+            List<int> storage = new List<int>();
+
+            void listHelper(Node helper, List<int> storage)
+            {
+                if (helper == null)
+                {
+                    return;
+                }
+
+                storage.Add(helper.Value);
+                listHelper(helper.Left, storage);
+                listHelper(helper.Right, storage);
+            }
+
+            listHelper(root, storage);
+            return storage;
+        }
+
+        public List<int> InOrder()
+        {
+            Node Root = root;
+            List<int> storage = new List<int>();
+
+            void listHelper(Node helper, List<int> storage)
+            {
+                if (helper == null)
+                {
+                    return;
+                }
+
+                listHelper(helper.Left, storage);
+                storage.Add(helper.Value);
+                listHelper(helper.Right, storage);
+            }
+
+            listHelper(root, storage);
+            return storage;
+        }
+        public List<int> PostOrder()
+        {
+            Node Root = root;
+            List<int> storage = new List<int>();
+
+            void listHelper(Node helper, List<int> storage)
+            {
+                if (helper == null)
+                {
+                    return;
+                }
+
+                listHelper(helper.Left, storage);
+                listHelper(helper.Right, storage);
+                storage.Add(helper.Value);
+            }
+
+            listHelper(root, storage);
+            return storage;
+        }
+    }
+}
