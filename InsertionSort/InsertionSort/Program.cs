d8604361cb8dd6717d374233710bb55b9d7f19e9
carrington <carringtonbeard@live.com>
2020-04-20 19:14:58 -0700

    tested my own array

--- InsertionSort/InsertionSort/Program.cs
+++ InsertionSort/InsertionSort/Program.cs
@@ -11,10 +11,12 @@ namespace InsertionSort
             int[] Reverse = { 20, 18, 12, 8, 5, -2 };
             int[] Uniques = { 5, 12, 7, 5, 5, 7 };
             int[] NearlySorted = { 2, 3, 5, 7, 13, 11 };
+            int[] MyOwnArray = { 4, 20, 420, 69, 24 };
 
             Console.WriteLine("[" + string.Join(", ", InsertionSort(Reverse)) + " ]");
             Console.WriteLine("[" + string.Join(", ", InsertionSort(Uniques)) + " ]");
             Console.WriteLine("[" + string.Join(", ", InsertionSort(NearlySorted)) + " ]");
+            Console.WriteLine("[" + string.Join(", ", InsertionSort(MyOwnArray)) + " ]");
         }
 
       public static int[] InsertionSort(int[] arr)
