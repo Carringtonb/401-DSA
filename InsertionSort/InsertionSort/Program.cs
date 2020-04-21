ab8922edf985b1d62073a26a9807db4e1a82d3f4
carrington <carringtonbeard@live.com>
2020-04-20 19:11:44 -0700

    coded implementation

---
+++ InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] Reverse = { 20, 18, 12, 8, 5, -2 };
+            int[] Uniques = { 5, 12, 7, 5, 5, 7 };
+            int[] NearlySorted = { 2, 3, 5, 7, 13, 11 };
+
+            Console.WriteLine("[" + string.Join(", ", InsertionSort(Reverse)) + " ]");
+            Console.WriteLine("[" + string.Join(", ", InsertionSort(Uniques)) + " ]");
+            Console.WriteLine("[" + string.Join(", ", InsertionSort(NearlySorted)) + " ]");
+        }
+
+      public static int[] InsertionSort(int[] arr)
+        {
+        for (int i = 1; i<arr.Length; i++)
+        {
+                int j = i - 1;
+                int temp = arr[i];
+
+                while (j >=0 && temp < arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    j -= 1;
+                }
+
+                arr[j + 1] = temp;
+        }
+
+            return arr;
+
+}
+    }
+}
