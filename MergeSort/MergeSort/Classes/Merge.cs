d4093188cb6b3d3ebfe736b591aa34f4095d6510
carrington <carringtonbeard@live.com>
2020-04-21 21:04:29 -0700

    walkthrough and implementation, tests to come later

---
+++ MergeSort/MergeSort/Classes/Merge.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace MergeSort.Classes
+{
+    //public class Merge
+    //{
+    //    public Merge(int[] left, int[] right, int[] original)
+    //    {
+    //        int i = 0;
+    //        int j = 0;
+    //        int k = 0;
+
+    //        while (i < left.Length && j < right.Length)
+    //        {
+    //            if (left[i] <= right[j])
+    //            {
+    //                original[k] = left[i];
+    //                i++;
+    //            }
+    //            else
+    //            {
+    //                original[k] = right[j];
+    //                j++;
+    //            }
+    //            k++;
+    //        }
+
+    //        if (i == left.Length)
+    //        {
+    //            while (j < right.Length)
+    //            {
+    //                original[k] = right[j];
+    //                j++;
+    //                k++;
+    //            }
+    //        }
+    //        else
+    //        {
+    //            while (i < left.Length)
+    //            {
+    //                original[k] = left[i];
+    //                i++;
+    //                k++;
+    //            }
+    //        }
+    //    }
+    //}
+}
